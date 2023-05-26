9af3a8b83c25f7b3237fb7feb9ecec6199ae06bc
Michaelszeng <michaelszeng@gmail.com>
2023-05-26 16:08:40 -0400

    functional scan, need to make it continuous

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(scan, CONFIG_LOG_DEFAULT_LEVEL);
 
+
 #include <nrfx_clock.h>
 #include <zephyr/kernel.h>
 #include <stdio.h>
@@ -27,34 +28,113 @@ LOG_MODULE_REGISTER(scan, CONFIG_LOG_DEFAULT_LEVEL);
 
 #define WIFI_SHELL_MODULE "wifi"
 
-#define WIFI_SHELL_MGMT_EVENTS (NET_EVENT_WIFI_SCAN_RESULT |		\
-				NET_EVENT_WIFI_SCAN_DONE)
+// #define WIFI_SHELL_MGMT_EVENTS (NET_EVENT_WIFI_SCAN_RESULT | NET_EVENT_WIFI_SCAN_DONE)
+#define WIFI_SHELL_MGMT_EVENTS (NET_EVENT_WIFI_SCAN_DONE |		\
+								NET_EVENT_WIFI_RAW_SCAN_RESULT)
 
 static uint32_t scan_result;
+static int scan_finished;
 
 static struct net_mgmt_event_callback wifi_shell_mgmt_cb;
 
-static void handle_wifi_scan_result(struct net_mgmt_event_callback *cb)
+static int wifi_freq_to_channel(int frequency)
 {
-	const struct wifi_scan_result *entry =
-		(const struct wifi_scan_result *)cb->info;
-	uint8_t mac_string_buf[sizeof("xx:xx:xx:xx:xx:xx")];
+	int channel = 0;
 
-	scan_result++;
-
-	if (scan_result == 1U) {
-		printk("%-4s | %-32s %-5s | %-4s | %-4s | %-5s | %s\n",
-		       "Num", "SSID", "(len)", "Chan", "RSSI", "Security", "BSSID");
+	if ((frequency <= 2424) && (frequency >= 2401)) {
+		channel = 1;
+	} else if ((frequency <= 2453) && (frequency >= 2425)) {
+		channel = 6;
+	} else if ((frequency <= 2484) && (frequency >= 2454)) {
+		channel = 11;
+	} else if ((frequency <= 5320) && (frequency >= 5180)) {
+		channel = ((frequency - 5180) / 5) + 36;
+	} else if ((frequency <= 5720) && (frequency >= 5500)) {
+		channel = ((frequency - 5500) / 5) + 100;
+	} else if ((frequency <= 5895) && (frequency >= 5745)) {
+		channel = ((frequency - 5745) / 5) + 149;
+	} else {
+		channel = frequency;
 	}
 
-	printk("%-4d | %-32s %-5u | %-4u | %-4d | %-5s | %s\n",
-	       scan_result, entry->ssid, entry->ssid_length,
-	       entry->channel, entry->rssi,
-	       (entry->security == WIFI_SECURITY_TYPE_PSK ? "WPA/WPA2" : "Open    "),
-	       ((entry->mac_length) ?
-			net_sprint_ll_addr_buf(entry->mac, WIFI_MAC_ADDR_LEN, mac_string_buf,
-						sizeof(mac_string_buf)) : ""));
+	return channel;
 }
+
+static enum wifi_frequency_bands wifi_freq_to_band(int frequency)
+{
+	enum wifi_frequency_bands band = WIFI_FREQ_BAND_2_4_GHZ;
+
+	if ((frequency  >= 2401) && (frequency <= 2495)) {
+		band = WIFI_FREQ_BAND_2_4_GHZ;
+	} else if ((frequency  >= 5170) && (frequency <= 5895)) {
+		band = WIFI_FREQ_BAND_5_GHZ;
+	} else {
+		band = WIFI_FREQ_BAND_6_GHZ;
+	}
+
+	return band;
+}
+
+static void handle_wifi_raw_scan_result(struct net_mgmt_event_callback *cb)
+{
+	LOG_WRN("IN WIFI SCAN RESULT HANDLER");
+	struct wifi_raw_scan_result *raw =
+		(struct wifi_raw_scan_result *)cb->info;
+	int channel;
+	int band;
+	int rssi;
+	uint8_t mac_string_buf[sizeof("xx:xx:xx:xx:xx:xx")];
+
+	// scan_result++;
+
+	// if (scan_result == 1U) {
+	// 	LOG_INF("%-4s | %-13s | %-4s |  %-15s | %-15s \n",
+	// 	      "Num", "Channel (Band)", "RSSI", "BSSID", "Frame length");
+	// }
+
+	rssi = raw->rssi;
+	channel = wifi_freq_to_channel(raw->frequency);
+	band = wifi_freq_to_band(raw->frequency);
+
+	// RSSI FILTER FOR TESTING (-30 should be detect the 2 devices if they are right next to each other, and not detect anything else)
+	if (rssi > -30) {
+		LOG_INF("%-4d | %-4u (%-6s) | %-4d | %s |      %-4d        ",
+			scan_result,
+			channel,
+			wifi_band_txt(band),
+			rssi,
+			net_sprint_ll_addr_buf(raw->data + 10, WIFI_MAC_ADDR_LEN, mac_string_buf, sizeof(mac_string_buf)), raw->frame_length);
+
+		
+		// LOG_HEXDUMP_INF(raw->data, sizeof(raw->data), "Frame Body: ");
+		printk("\n\n");
+	}
+
+	
+}
+
+// static void handle_wifi_scan_result(struct net_mgmt_event_callback *cb)
+// {
+// 	printk("\n");
+// 	const struct wifi_scan_result *entry =
+// 		(const struct wifi_scan_result *)cb->info;
+// 	uint8_t mac_string_buf[sizeof("xx:xx:xx:xx:xx:xx")];
+
+// 	scan_result++;
+
+// 	if (scan_result == 1U) {
+// 		printk("%-4s | %-32s %-5s | %-4s | %-4s | %-5s | %s\n",
+// 		       "Num", "SSID", "(len)", "Chan", "RSSI", "Security", "BSSID");
+// 	}
+
+// 	printk("%-4d | %-32s %-5u | %-4u | %-4d | %-5s | %s\n",
+// 	       scan_result, entry->ssid, entry->ssid_length,
+// 	       entry->channel, entry->rssi,
+// 	       (entry->security == WIFI_SECURITY_TYPE_PSK ? "WPA/WPA2" : "Open    "),
+// 	       ((entry->mac_length) ?
+// 			net_sprint_ll_addr_buf(entry->mac, WIFI_MAC_ADDR_LEN, mac_string_buf,
+// 						sizeof(mac_string_buf)) : ""));
+// }
 
 static void handle_wifi_scan_done(struct net_mgmt_event_callback *cb)
 {
@@ -64,6 +144,7 @@ static void handle_wifi_scan_done(struct net_mgmt_event_callback *cb)
 	if (status->status) {
 		LOG_ERR("Scan request failed (%d)", status->status);
 	} else {
+		scan_finished = 1;  // set global variable to indicate scanning is not longer in progress
 		LOG_INF("Scan request done");
 	}
 
@@ -74,8 +155,11 @@ static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
 				     uint32_t mgmt_event, struct net_if *iface)
 {
 	switch (mgmt_event) {
-	case NET_EVENT_WIFI_SCAN_RESULT:
-		handle_wifi_scan_result(cb);
+	// case NET_EVENT_WIFI_SCAN_RESULT:
+	// 	handle_wifi_scan_result(cb);
+	// 	break;
+	case NET_EVENT_WIFI_RAW_SCAN_RESULT:
+		handle_wifi_raw_scan_result(cb);
 		break;
 	case NET_EVENT_WIFI_SCAN_DONE:
 		handle_wifi_scan_done(cb);
@@ -87,6 +171,8 @@ static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
 
 static int wifi_scan(void)
 {
+	scan_finished = 0;  // set global variable to indicate scanning currently in progresss
+
 	struct net_if *iface = net_if_get_default();
 
 	if (net_mgmt(NET_REQUEST_WIFI_SCAN, iface, NULL, 0)) {
@@ -95,13 +181,15 @@ static int wifi_scan(void)
 		return -ENOEXEC;
 	}
 
-	LOG_INF("Scan requested");
+	// LOG_INF("Scan requested");
 
 	return 0;
 }
 
 int main(void)
 {
+	LOG_INF("==================================PROGRAM STARTING==================================");
+
 	scan_result = 0U;
 
 	net_mgmt_init_event_callback(&wifi_shell_mgmt_cb,
@@ -115,10 +203,38 @@ int main(void)
 	nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK,
 			       NRF_CLOCK_HFCLK_DIV_1);
 #endif
-	k_sleep(K_SECONDS(1));
-	LOG_INF("Starting %s with CPU frequency: %d MHz", CONFIG_BOARD, SystemCoreClock / MHZ(1));
+	// k_sleep(K_SECONDS(1));
+	// LOG_INF("Starting %s with CPU frequency: %d MHz", CONFIG_BOARD, SystemCoreClock / MHZ(1));  // should be nrf7002dk_nrf5340_cpuapp with 128 MHz
 
 	wifi_scan();
+
+	// scan_finished = 1;
+
+	// while(1) {
+	// 	LOG_WRN("ENTERED LOOP");
+	// 	if (scan_finished == 1) {  // only perform a scan if not anothr scan is currently in progress
+	// 		LOG_WRN("BEGINNING SCAN");
+	// 		wifi_scan();
+	// 	}	
+	// }
+
+	// LOG_INF("EXITED LOOP");
+	
+
+
+	// int MAX_NUM_SCANS = 10;
+	// int count = 0;
+	// while(1) {
+	// 	k_sleep(K_SECONDS(1));
+	// 	// LOG_INF("Starting %s with CPU frequency: %d MHz", CONFIG_BOARD, SystemCoreClock / MHZ(1));  // should be nrf7002dk_nrf5340_cpuapp with 128 MHz
+
+	// 	wifi_scan();
+	// 	count += 1;
+
+	// 	if (count > MAX_NUM_SCANS) {
+	// 		break;
+	// 	}
+	// }
 
 	return 0;
 }
